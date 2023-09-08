f2151ceb7aca4d98d0198fe26b91c05bdbc12500
AliGZN <geramiali.ag@gmail.com>
2023-09-08 20:23:41 +0330

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int num1 =1;
+        int num2 =1;
+        int num3 =1;
+
+        PrintEqual.printEqual(num1,num2,num3);
+
+    }
+}
